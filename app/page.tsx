f5b4a2a0815b85626deb7c1d4a014ba19e5c7eff
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-04-19 15:54:17 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-import postgres from "postgres";
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
 
-import { AddForm } from "@/app/add-form";
-import { DeleteForm } from "@/app/delete-form";
-
-let sql = postgres(process.env.DATABASE_URL || process.env.POSTGRES_URL!, {
-  ssl: "allow",
-});
-
-export default async function Home() {
-  let todos = await sql`SELECT * FROM todos`;
-
-  return (
-    <main>
-      <h1 className="sr-only">Todos</h1>
-      <AddForm />
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.text}
-            <DeleteForm id={todo.id} todo={todo.text} />
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
-}
+export default HomePage;
